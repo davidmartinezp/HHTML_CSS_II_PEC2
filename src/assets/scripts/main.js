aa1c05c3134123be289c131a39001160a91fd2a9
David <david.martinez.perello@gmail.com>
2023-11-28 20:57:05 +0100

    acabada portada

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -5,8 +5,10 @@
 
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
-//import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+import 'jquery'; // Importar jQuery
+import 'bootstrap'; // Importar Bootstrap JavaScript
 /**
  * Write any other JavaScript below
  */
